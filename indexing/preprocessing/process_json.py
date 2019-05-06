f8a1090c15b45b023a09b3f050bbd84d63268256
petrosdeb <petrosd@student.chalmers.se>
2019-05-07 01:22:55 +0800

    Create dictionary based on tags

--- indexing/preprocessing/process_json.py
+++ indexing/preprocessing/process_json.py
@@ -2,14 +2,26 @@
 
 import json
 
-data = []
-with open('C:\SearchFlow\SearchFlow\crawler\_Data.json', encoding="utf8") as f:
-    for line in f:
-        data.append(json.loads(line))
 
-tags = []
-for word in data:
-    print(word['items'][0]['tags'])
-    tags.append(word['items'][0]['tags'])
+def tag_processing(file):
+    tags = set()
+    data = []
+    with open(file, encoding="utf8") as f:
+        for line in f:
+            data.append(json.loads(line))
+    for word in data:
+        for tag in word['items'][0]['tags']:
+            tags.add(tag)
 
-print(tags)
+    return tags
+
+
+def tag_file_edit(tags):
+    file = open('tags.txt', 'a')
+    for tag in tags:
+        file.write(tag + '\n')
+        print(tag)
+    file.close()
+
+
+tag_file_edit(tag_processing('C:\SearchFlow\SearchFlow\crawler\_Data.json'))
