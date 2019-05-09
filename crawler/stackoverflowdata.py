a7b0778f6a85a2a5f634e895179274da12d980e0
Pritom <naitiq@yahoo.com>
2019-05-09 18:04:29 +0800

    edited stackoveflowdata.py
    Signed-off-by: Pritom <naitiq@yahoo.com>

--- crawler/stackoverflowdata.py
+++ crawler/stackoverflowdata.py
@@ -3,16 +3,27 @@ from bs4 import BeautifulSoup
 from crawler.debug import debug
 from crawler.linkparser import LinkParser
 from crawler.linkparser import question_url_creator
+from crawler.mongodb import Connection
 
+
+# import platform
+# import sys
+# if platform.system().lower() in ['linux', 'darwin']:
+#     # sys.setdefaultencoding() does not exist, here!
+#     reload(sys)  # Reload does the trick!
+#     sys.setdefaultencoding('UTF8')
 
 class StackOverflowInfo:
 
-    def __init__(self, page_id):
+    def __init__(self, question_id):
         self.dbug = debug(name=self.__class__, flag=True)
-        self.page_soup = LinkParser.link_info(question_url_creator(page_id))
+        self.question_id = question_id
+        self.page_soup = LinkParser().link_info(url=question_url_creator(self.question_id))
 
-    def question_info(self):
-        return
+    def all_info(self):
+        question_info = QuestionInfo(self.question_id).get_all_question_info(self.page_soup)
+        answer_info = AnswersInfo(self.question_id).get_all_answer_info(self.page_soup)
+        return {"Question": question_info, "Answer": answer_info}
 
 
 class QuestionInfo:
@@ -23,7 +34,7 @@ class QuestionInfo:
 
     def get_all_question_info(self, page_soup):
         question_soup = BeautifulSoup(str(page_soup.find("div", {"class": "question"})))
-        self.question_title = str(page_soup.find("title").text).split("-")[1].encode('utf-8').strip()
+        self.question_title = str(page_soup.find("title").text).split("-")[1].strip()
         self.question_ask_time = page_soup.find("time").get("datetime")
         self.question_views = self.question_views(page_soup)
         self.question_tags = self.question_tags(question_soup)
@@ -32,6 +43,15 @@ class QuestionInfo:
         self.question_text = str(question_soup.find("div", {"class": "post-text"}).text).strip()
         self.question_code = self.question_code(question_soup)
         self.related_questions = self.related_questions(page_soup)
+
+        question_json = {"question_id": self.question_id, "question_title": self.question_title,
+                         "question_asked_time": self.question_ask_time,
+                         "question_tags": self.question_tags, "question_views": self.question_views,
+                         "question_upvote": self.question_upvote,
+                         "question_text": self.question_text, "question_code": self.question_code,
+                         "related_questions": self.related_questions}
+        self.dbug.debug_print(question_json)
+        return question_json
 
     def question_views(self, soup):
         views = soup.find("p", {"class": "label-key"}).text
@@ -67,10 +87,14 @@ class AnswersInfo:
         self.dbug = debug(name=self.__class__, flag=True)
         self.question_id = question_id
 
-    def get_all_question_info(self, soup):
+    def get_all_answer_info(self, soup):
         answer_soup = BeautifulSoup(str(soup.find("div", {"id": "answers"})))
         self.total_answers = answer_soup.find("h2").get("data-answercount")
         self.answer_details, self.answer_code = self.answers_detail(self.total_answers, answer_soup)
+        answer_json = {"total_answers": self.total_answers, "answer_code": self.answer_code,
+                       "answers": self.answer_details}
+        self.dbug.debug_print(answer_json)
+        return answer_json
 
     def answers_detail(self, total_answers, answer_soup):
         answers_array = []
@@ -83,11 +107,17 @@ class AnswersInfo:
                 ans_dict = {'answer_upvote': answer_soup.find_all("div", {
                     "class": "js-vote-count grid--cell fc-black-500 fs-title grid fd-column ai-center"})[ans].text,
                             'answer': answer_soup.find_all("div", {"class": "post-text"})[ans].text}
-                if answer_soup.find_all("div",
-                                        {"class": "js-accepted-answer-indicator grid--item fc-green-500 ta-center p4"})[
-                    ans] != None:
-                    ans_dict['accepted'] = True
+                if answer_soup.find_all("div", {"class": "answer"})[ans].find("div", {
+                    "class": "js-accepted-answer-indicator grid--item fc-green-500 ta-center p4"}) is not None:
+                    ans_dict['answer_accepted'] = True
                 else:
-                    ans_dict['accepted'] = False
+                    ans_dict['answer_accepted'] = False
                 answers_array.append(ans_dict)
         return answers_array, answer_code_arr
+
+
+connection = Connection(db_name="StackOverflow", db_col="Final_Test_Question_URL")
+test = StackOverflowInfo(56056337)
+info = test.all_info()
+print(info)
+connection.db_col.insert(info)
