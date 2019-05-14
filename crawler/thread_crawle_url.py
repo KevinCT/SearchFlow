6f8374c5449c47ef524e3309851b0487cc452ea4
Pritom007 <naitiq@yahoo.com>
2019-05-14 15:27:44 +0800

    just committed the previous codde

--- crawler/thread_crawle_url.py
+++ crawler/thread_crawle_url.py
@@ -7,11 +7,12 @@ from crawler.linkparser import page_url_creator
 from crawler.mongodb import Connection
 
 db_connection = Connection(db_name="StackOverflow", db_col="Multi_Thread_URL")
-START_PAGE = 0
-END_PAGE = 150000
+START_PAGE = 290000
+END_PAGE = 2000
 TOTAL_PAGES = END_PAGE - START_PAGE
-Thread_Num = 8
 
+
+# this is the function invoked by the thread
 def thread_url(page_id):
     debug(thread_url, flag=True).debug_print("Thread %s: starting" + threading.current_thread().name)
     page_url = page_url_creator(page_id=page_id)
@@ -21,45 +22,37 @@ def thread_url(page_id):
     debug(thread_url, flag=True).debug_print("Thread %s: finishing" + threading.current_thread().name)
 
 
+# processing the data to insert it in the database
 def data_process_for_db(page_id, data):
+    print(len(data))
     for d in data:
         info = {"Question": d, "Answer": {}, "crawled": False}
         db_connection.insert(info)
     debug(data_process_for_db, flag=True).debug_print("Insert Done for page " + str(page_id))
 
-
 if __name__ == '__main__':
     threads = list()
     time1 = time.time()
-    for count in range(END_PAGE, START_PAGE, -8):
-        t1 = threading.Thread(target=thread_url, args=(count - 1,))
-        t2 = threading.Thread(target=thread_url, args=(count - 2,))
-        t3 = threading.Thread(target=thread_url, args=(count - 3,))
-        t4 = threading.Thread(target=thread_url, args=(count - 4,))
-        t5 = threading.Thread(target=thread_url, args=(count - 5,))
-        t6 = threading.Thread(target=thread_url, args=(count - 6,))
-        t7 = threading.Thread(target=thread_url, args=(count - 7,))
-        t8 = threading.Thread(target=thread_url, args=(count - 8,))
+
+    for count in range(START_PAGE, END_PAGE, -4):
+        t1 = threading.Thread(target=thread_url, args=(count + 1,))
+        t2 = threading.Thread(target=thread_url, args=(count + 2,))
+        t3 = threading.Thread(target=thread_url, args=(count + 3,))
+        t4 = threading.Thread(target=thread_url, args=(count + 4,))
 
         threads.append(t1)
         threads.append(t2)
         threads.append(t3)
         threads.append(t4)
-        threads.append(t5)
-        threads.append(t6)
-        threads.append(t7)
-        threads.append(t8)
 
         t1.start()
         t2.start()
         t3.start()
         t4.start()
-        t5.start()
-        t6.start()
-        t7.start()
-        t8.start()
 
+        if count % 50 == 0:
+            print("Going to sleep")
+            time.sleep(1)
         for index, thread in enumerate(threads):
             thread.join()
-        count += 1
     print("Final Time:", time.time() - time1)
--- crawler/thread_info_url.py
+++ crawler/thread_info_url.py
@@ -8,17 +8,19 @@ from crawler.stackoverflowdata import StackOverflowInfo
 db_connection = Connection(db_name="StackOverflow", db_col="Multi_Thread_URL")
 
 
+# this the function invoked by the thread
 def thread_info_url(question_id):
-    debug(thread_info_url, flag=True).debug_print("Thread " + threading.current_thread().name + " starting")
+    debug(thread_info_url, flag=True).debug_print("Thread %s: starting" + threading.current_thread().name)
     data_process_for_db(question_id=question_id)
     time.sleep(1)
-    debug(thread_info_url, flag=True).debug_print("Thread " + threading.current_thread().name + " finishing")
+    debug(thread_info_url, flag=True).debug_print("Thread %s: finishing" + threading.current_thread().name)
 
 
+# updating the info in the db
 def data_process_for_db(question_id):
     stackoverflow_info = StackOverflowInfo(question_id=question_id)
     data = stackoverflow_info.all_info()
-    if len(data['Question'].get('question_title')) > 2:
+    if len(str(data['Question'].get('question_title'))) > 2:
         if db_connection.db_col.update_one({'Question.question_id': question_id, 'crawled': False}, {
             '$set': {'Question': data['Question'], 'Answer': data['Answer'], 'crawled': True}}):
             debug(data_process_for_db, flag=True).debug_print("Insert Done for question " + str(question_id))
@@ -27,23 +29,21 @@ def data_process_for_db(question_id):
     else:
         pass
 
+
 if __name__ == '__main__':
     threads = list()
     time1 = time.time()
     while db_connection.db_col.find({'crawled': False}).count() > 0:
-        mylist = db_connection.db_col.find({'crawled': False}).limit(3)
-        data = [x['Question'].get('question_id') for x in mylist]
+        data_list = db_connection.db_col.find({'crawled': False}).limit(2)
+        data = [x['Question'].get('question_id') for x in data_list]
         t1 = threading.Thread(target=thread_info_url, args=(data[0],))
         t2 = threading.Thread(target=thread_info_url, args=(data[1],))
-        t3 = threading.Thread(target=thread_info_url, args=(data[1],))
 
         threads.append(t1)
         threads.append(t2)
-        threads.append(t3)
 
         t1.start()
         t2.start()
-        t3.start()
 
         for index, thread in enumerate(threads):
             thread.join()
