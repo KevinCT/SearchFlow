2d12aad19830c5c95756ebca6644ceca504fce51
KevinCT <kevinchentrieu@gmail.com>
2019-05-15 14:37:28 +0800

    change data structures for scoring

--- searchflow/searchengine/scoring.py
+++ searchflow/searchengine/scoring.py
@@ -10,14 +10,11 @@ def cosineSimilarity(a, b):
 
 
 def termFrequency(termsDictionary, document):
-    document = document.split()
+    documentLength = sum(document.values())
     frequencyDictionary = termsDictionary.copy()
     for term in document:
         if term in frequencyDictionary.keys():
-            frequencyDictionary[term] += 1
-
-    for term in frequencyDictionary:
-        frequencyDictionary[term] = frequencyDictionary[term]/len(document)
+            frequencyDictionary[term] = document.get(term)/documentLength
     return frequencyDictionary
 
 
@@ -26,10 +23,11 @@ def tfidf(idfDictionary, documents):
     tfidfList = []
     terms = set()
     for document in documents:
-        terms.update(document.split())
+        print(document)
+        terms.update(documents[document].keys())
     termsDictionary = dict.fromkeys(terms, 0)
     for document in documents:
-        tfDictionaries[document] = (termFrequency(termsDictionary, document))
+        tfDictionaries[document] = (termFrequency(termsDictionary, documents[document]))
     for tfDictionary in tfDictionaries:
         tfDictionary = tfDictionaries[tfDictionary]
         tempDictionary = {}
@@ -39,9 +37,17 @@ def tfidf(idfDictionary, documents):
 
     return tfidfList
 
-#documents is a list of strings where each string is the text.
+#documents is a dictionary of dictionary containing frequencies of each term
+#query is a list of strings containing the terms
 def getScore(idfDictionary, documents, query):
-    documents.append(query)
+    queryDictionary = {}
+    for term in query:
+        if term in queryDictionary:
+            queryDictionary[term] += 1
+        else:
+            queryDictionary[term] = 1
+
+    documents['query'] = queryDictionary
     tfidfList = tfidf(idfDictionary, documents)
     vectorList = []
     scoreList = []
@@ -53,7 +59,15 @@ def getScore(idfDictionary, documents, query):
     return scoreList
 
 
+def test():
+    doc1 = "django is a web framework for python"
+    doc2 = "bootstrap is a popular web framework"
 
+    documents = {}
+    documents['doc1'] = dict.fromkeys(doc1.split(), 1)
+    documents['doc2'] = dict.fromkeys(doc2.split(), 1)
 
-#print(getScore( {'a': 1.0, 'framework': 1.0, 'is': 1.0, 'django': 1.6931471805599454, 'web': 1.0, 'for': 1.6931471805599454, 'python': 1.6931471805599454, 'popular': 1.6931471805599454, 'bootstrap': 1.6931471805599454}
-#, ["django is a web framework for python", "bootstrap is a popular web framework"], "python framework" ))
+    print(getScore( {'a': 1.0, 'framework': 1.0, 'is': 1.0, 'django': 1.6931471805599454, 'web': 1.0, 'for': 1.6931471805599454, 'python': 1.6931471805599454, 'popular': 1.6931471805599454, 'bootstrap': 1.6931471805599454}
+    , documents, ["python", "framework"]))
+
+#test()
