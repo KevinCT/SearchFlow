558590d1297ca02c83a96a5442b35c94d86b5667
KevinCT <kevinchentrieu@gmail.com>
2019-06-04 13:32:16 +0800

    Add functionality for tags and sorting by option

--- searchflow/searchengine/views.py
+++ searchflow/searchengine/views.py
@@ -2,9 +2,63 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from crawler.extrafunctions import *
+from django.core.paginator import Paginator
+from django.shortcuts import render
+
 # Create your views here.
 
 def index(request):
+    topTags = getTags()
+    template = loader.get_template('index.html')
+    context = {
+        'tag_list': topTags
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def query(request):
+    if request.method == 'GET':
+        topTags = getTags()
+        query = request.GET.get('queryField', None)
+        option = request.GET.get('optionSelector', None)
+        tags = [request.GET.get('tOne'), request.GET.get('tTwo'), request.GET.get('tThree'), request.GET.get('tFour'),
+                request.GET.get('tFive')]
+        tags = list(filter(None, tags))
+
+        insertTop(query)
+        #tags is a list of tags, option is the way the result should be sorted(e.g. by answer, question, date..)
+        #method for returning data from backend required here. getData(query, tags, option) should return a list of tuples which contains (title, link, description)
+
+
+
+        resultList = []
+        for i in range (0, 40):
+          resultList.append((query + option + str(i), "http://www.stackoverflow.com", "This page shows how to remove the hyperlink underline with CSS by using the text-decoration property. Did you know that removing the underline allows you to ..."))
+    #    resultListBlock = [resultList[i * 10:(i + 1) * 10] for i in range((len(resultList) + 10 - 1) // 10)]
+        template = loader.get_template('results.html')
+
+        paginator = Paginator(resultList, 10)
+        page = request.GET.get('page')
+        users = paginator.get_page(page)
+        context = {
+            'users': users,
+            'tag_list': topTags,
+        }
+        return HttpResponse(template.render(context, request))
+
+    else:
+        return HttpResponse("Something went wrong")
+
+#            return listing(request, resultList)
+
+
+#def listing(request, resultList):
+#    paginator = Paginator(resultList, 10)
+#    page = request.GET.get('page')
+#    users = paginator.get_page(page)
+#    return render(request, 'results.html', {'users': users})
+
+def getTags():
     backupList = ["java", "python", "c++", "javascript", "android" ]
     topTags = topSearch()
     if len(topTags) >= 5:
@@ -14,28 +68,4 @@ def index(request):
         for i in range(length, 5):
             topTags.append(backupList[i])
         topTags = [tuple(topTags[:5])]
-    template = loader.get_template('index.html')
-    print(topTags)
-    context = {
-        'tag_list': topTags
-    }
-    return HttpResponse(template.render(context, request))
-
-
-def query(request):
-    if request.method == 'GET':
-        query = request.GET.get('queryField', None)
-        option = request.GET.get('optionSelector', None)
-        if query is not None:
-            insertTop(query)
-            #method for returning data from backend. getData(query) should return a list of tuples which contains (title, link, description)
-            resultList = [(query + option, "http://www.stackoverflow.com", "This page shows how to remove the hyperlink underline with CSS by using the text-decoration property. Did you know that removing the underline allows you to ...")]
-            template = loader.get_template('results.html')
-            context = {
-                'result_list': resultList,
-            }
-            return HttpResponse(template.render(context, request))
-        else:
-            return HttpResponse("Search cannot be empty")
-    else:
-        return HttpResponse("Something went wrong")
+    return topTags
